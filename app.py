6dbfab7bdac5b5d8bf6a6d2c61ab48d8763464c9
João Paulo <jpmdik@gmail.com>
2019-06-20 16:06:58 -0300

    [FIX] unicode

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# coding=utf-8
 
 import os
 if os.path.isfile('.env'):
@@ -10,7 +10,7 @@ import requests
 import dropbox
 import logging
 
-from flask import Flask, Response, render_template, url_for
+from flask import Flask, Response, render_template, url_for, send_file
 from flask import request
 
 app = Flask(__name__)
@@ -144,15 +144,8 @@ def downloadArquivo(nome_diretorio, nome):
     checarDiretorio()
     try:
       folder = PATH_DOWNLOAD+nome_diretorio+"-"+nome
-      client.files_download_to_file(folder, "/"+nome_diretorio+"/"+nome)
-      with open(folder, 'rb') as arquivo:
-        csv = arquivo.read()
-      os.remove(folder)
-      return Response(
-          csv,
-          mimetype="text/csv",
-          headers={"Content-disposition":
-                   "attachment; filename="+nome})
+      client.files_download_to_file(folder, "/"+nome_diretorio+"/"+nome)      
+      return send_file(folder, as_attachment=True)
     except Exception as err:
       print(err)
       return render_template('erro.html', erro='O arquivo foi movido ou removido.',nome=nome_diretorio)
