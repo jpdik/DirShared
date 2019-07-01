82cd23a8456f9aef6a953462fc96f8c27ab0e909
João Paulo <jpmdik@gmail.com>
2019-07-01 00:26:47 -0300

    [MOD] url aplication

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const consts = {
     MAX_MB: 10,
-    URL: 'http://localhost:3003/api',
+    URL: 'https://dirshared-back.herokuapp.com/api',
     NOT_PERMITED_FILES: ['mp3', 'wma', 'wav', 'm4a', 'mov', 'avi', 'mpg', 'mpeg', 'ogg']
 }
