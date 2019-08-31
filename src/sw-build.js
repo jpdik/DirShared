684a5e9de38753ced6244784eb5dc8e9761b39ea
Joao Paulo de Melo <jpmdik@gmail.com>
2019-08-31 19:39:52 -0300

    [FIX] sw

--- src/sw-build.js
+++ src/sw-build.js
@@ -9,7 +9,7 @@ const buildSW = () => {
     swDest: 'build/sw.js', // this will be created in the build step
     globDirectory: 'build',
     globPatterns: [
-      '**\/*.{js,css,html,png,json,svg,ico}',
+      '**\/*.{js,css,html,png,json,svg,ico,woff,woff2,ttf}',
     ]
   }).then(({count, size, warnings}) => {
     // Optionally, log any warnings and details.
